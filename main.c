e8b7b3a35b8c16ce11917672fcc34361dcdf99f5
Jaehwang Kim <kim.jaehwang@gmail.com>
2024-09-14 12:01:16 +0900

    Simple version done

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
+void bar() {
+}
+
+void foo() {
+    bar();
+}
 int main() {
     printf("Hello, world!\n");
+    foo();
     return 0;
 }
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,26 @@ use clang::{Clang, Index};
 // entity를 traverse하는 함수
 fn traverse_entity(entity: &clang::Entity) {
     // 현재 entity 출력
-    println!("{:?}", entity.get_kind());
-    println!("{:?}", entity.get_location());
+    if entity.get_kind() == clang::EntityKind::FunctionDecl || entity.get_kind() == clang::EntityKind::CallExpr {
+        // println!("{:?}", entity.get_kind());
+        // println!("{:?}", entity.get_location());
+        
+        if entity.get_kind() == clang::EntityKind::FunctionDecl {
+            if let (Some(name), Some(location)) = (entity.get_name(), entity.get_location()) {
+                if let Some(file) = location.get_file_location().file {
+                    println!("Caller: {:?} {:?}", name, file.get_path());
+                }
+            }
+        } else if entity.get_kind() == clang::EntityKind::CallExpr {
+            if let Some(function) = entity.get_reference() {
+                if let Some(name) = function.get_name() {
+                    println!("  Callee: {:?}", name);
+                }
+            }
+        } else {
+            //pass
+        }
+    }
 
     // 자식 entity들을 traverse
     for child in entity.get_children() {
@@ -18,7 +36,10 @@ fn main() {
     let index = Index::new(&clang, false, false);
 
     // main.c 파일 파싱
-    let tu = index.parser("main.c").parse().unwrap();
+    let tu = index.parser("main.c")
+        .arguments(&["-I", "/path/to/include", "-D", "DEBUG"])
+        .parse()
+        .unwrap();
 
     traverse_entity(&tu.get_entity());
 }
