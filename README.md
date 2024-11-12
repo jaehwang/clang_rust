85a42d0cd35f14241babad3de1d216c79c807941
Jaehwang Kim <jaehwang.kim@lge.com>
2024-11-12 21:58:57 +0900

    Update README.md and build.rs
    - Updated README.md with new instructions and information.
    - Modified build.rs to improve build process and fix issues.

--- README.md
+++ README.md
@@ -2,6 +2,20 @@
 
 Clang을 이용하여 C/C++ 코드의 호출 그래프를 생성하는 프로그램
 
+## Homebrew를 쓰는 Linux에서 빌드 할 때
+
+libclang.so를 설치해야 한다.
+```sh
+brew install llvm
+```
+
+clang_sys를 빌드 할 때 library 위치를 알려 줘야 한다.
+```sh
+export LIBCLANG_PATH=/home/linuxbrew/.linuxbrew/Cellar/llvm/19.1.3/lib
+```
+
+`build.rs`에서 `rustc-link-arg=-Wl,-rpath`를 지정해야 한다.
+
 <!--
 vim:nospell
 -->
--- build.rs
+++ build.rs
@@ -3,7 +3,13 @@
 
 fn main() {
     // Clang 라이브러리 경로
-    let lib_path = "/Applications/Xcode.app/Contents/Frameworks";
+    let lib_path = if cfg!(target_os = "macos") {
+        "/Applications/Xcode.app/Contents/Frameworks"
+    } else if cfg!(target_os = "linux") {
+        "/home/linuxbrew/.linuxbrew/Cellar/llvm/19.1.3/lib"
+    } else {
+        panic!("Unsupported OS");
+    };
 
     // rpath 설정
     println!("cargo:rustc-link-arg=-Wl,-rpath,{}", lib_path);
